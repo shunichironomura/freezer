ff0181f3235a6b2bd5c083e19926baa99f8d3305
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2020-07-14 16:24:24 +0900

    add classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ setup(
     author='Shunichiro Nomura',
     author_email='nomura@space.t.u-tokyo.ac.jp',
     install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
     url='https://github.com/shunichironomura/freezer',
     license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
