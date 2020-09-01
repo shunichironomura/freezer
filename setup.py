32730ccd13427d1919d38e2b45e86a3ee0b0e2d4
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2020-09-01 14:50:40 +0900

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='object-freezer',
-    version='0.1.1',
+    version='0.1.2',
     description='Python module for freezing objects',
     long_description=readme(),
     long_description_content_type='text/markdown',
